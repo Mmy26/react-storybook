4a733d8c1ff130fb2c4377c3ba544606326900cc
Mmy26 <yu.b1994masamura@gmail.com>
2023-05-13 23:37:52 +0900

    [add]buttonサイズ違いの追加

--- src/components/Button/Button.stories.jsx
+++ src/components/Button/Button.stories.jsx
@@ -10,3 +10,13 @@ export const ClickButton = () => <Button>Click!</Button>;
 export const Default = () => <Button>Default</Button>;
 export const Primary = () => <Button color="primary">Primary</Button>;
 export const Danger = () => <Button color="danger">Danger</Button>;
+export const PrimarySmall = () => (
+  <Button size="sm" color="primary">
+    Small
+  </Button>
+);
+export const PrimaryLarge = () => (
+  <Button size="lg" color="primary">
+    Large
+  </Button>
+);
