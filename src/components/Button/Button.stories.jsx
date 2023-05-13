bbb2024e37e128f656ac24b9d6e37e92c45e6cb6
Mmy26 <yu.b1994masamura@gmail.com>
2023-05-14 00:48:32 +0900

    [update]argTypesを利用しないでActions

--- src/components/Button/Button.stories.jsx
+++ src/components/Button/Button.stories.jsx
@@ -1,39 +1,29 @@
+import { action } from "@storybook/addon-actions";
 import Button from "./Button";
 
 export default {
-  title: "Common/Button",
+  title: "Common/Test",
   component: Button,
   argTypes: {
     color: {
       options: ["primary", "default", "danger"],
-      control: { type: "select" },
+      control: { type: "radio" },
     },
     size: {
       options: ["sm", "base", "lg"],
-      control: { type: "radio" },
-    },
-    backgroundColor: {
-      control: { type: "color" },
+      control: { type: "select" },
     },
   },
 };
 
-export const HelloButton = () => <Button>Hello World!</Button>;
-export const ClickButton = () => <Button>Click!</Button>;
-export const Default = () => <Button>Default</Button>;
-export const Primary = () => <Button color="primary">Primary</Button>;
-export const Danger = () => <Button color="danger">Danger</Button>;
-export const PrimarySmall = () => (
-  <Button size="sm" color="primary">
-    Small
-  </Button>
-);
-export const PrimaryLarge = () => (
-  <Button size="lg" color="primary">
-    Large
-  </Button>
-);
-const Template = (args) => <Button {...args} />;
+const something = action("something");
+
+const Template = (args) => {
+  const handleClick = () => {
+    something();
+  };
+  return <Button {...args} handleClick={handleClick} />;
+};
 export const DefaultArgs = Template.bind({});
 DefaultArgs.args = {
   children: "Default",
